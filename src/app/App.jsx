7fbcf6383332e82386c9d34294c01c25a82fe9d5
karlafehir <fehirkarla@gmail.com>
2025-04-15 15:29:44 +0200

    added postDetails to routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "../features/home/HomePage";
+import PostDetails from "../features/posts/PostDetails";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/posts/:postId",
+    element: <PostDetails />,
   },
 ]);
 
