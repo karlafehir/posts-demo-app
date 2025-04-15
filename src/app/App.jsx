02e4b86c78f49e8aec35235a4d0f41c88104f6ae
karlafehir <fehirkarla@gmail.com>
2025-04-15 14:45:16 +0200

    added router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "../features/home/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/posts",
+    element: <HomePage />,
+  },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <HomePage />
+      <RouterProvider router={router} />
     </>
   );
 }
